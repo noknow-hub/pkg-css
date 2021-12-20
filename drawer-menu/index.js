2d2324d382d49f7e3dfa133fc0a7a0d0bc39311d
Kakeru Sato <sato.kakeru3@gmail.com>
2021-12-21 03:19:07 +0400

    pre-release

--- drawer-menu/index.js
+++ drawer-menu/index.js
@@ -34,13 +34,12 @@ class DrawerMenu {
             targetElm.addEventListener('click', (event) => {
                 event.preventDefault();
                 event.stopPropagation();
-                const targetVal = event.target.dataset[DrawerMenu.DATE_DRAWER_MENU_TARGET_VAL];
-console.log(targetVal);
+                const targetVal = targetElm.dataset[DrawerMenu.DATE_DRAWER_MENU_TARGET_VAL];
                 for(let j = 0; j < idElms.length; j++) {
                     const idElm = idElms[j];
                     const idVal = idElm.dataset[DrawerMenu.DATE_DRAWER_MENU_ID_VAL];
                     if(targetVal === idVal) {
-                        event.target.classList.toggle(DrawerMenu.ACTIVE_CLASS_NAME);
+                        idElm.classList.toggle(DrawerMenu.ACTIVE_CLASS_NAME);
                         break;
                     }
                 }
@@ -50,8 +49,8 @@ console.log(targetVal);
         if(document.body.nkwDrawerMenuEventListener === undefined && document.body.nkwDrawerMenuEventListener === null || !document.body.nkwDrawerMenuEventListener) {
             document.body.nkwDrawerMenuEventListener = true;
             document.body.addEventListener('click', (event) => {
-                for(let i = 0; i < targetElms.length; i++) {
-                    const elm = targetElms[i];
+                for(let i = 0; i < idElms.length; i++) {
+                    const elm = idElms[i];
                     if(elm.classList.contains(DrawerMenu.ACTIVE_CLASS_NAME)) {
                         let isInElm = false;
                         for(let e = event.target; e !== undefined && e !== null; e = e.parentElement) {
