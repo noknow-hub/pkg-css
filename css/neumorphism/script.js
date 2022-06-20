91a19767b500f9156ed6acd063c4b160c4e518c7
Kakeru Sato <sato.kakeru3@gmail.com>
2022-06-20 17:12:40 +0300

    pre-release

---
+++ css/neumorphism/script.js
@@ -0,0 +1,68 @@
+//////////////////////////////////////////////////////////////////////
+// neumorphism.js
+//////////////////////////////////////////////////////////////////////
+class Neumorphism {
+
+    //////////////////////////////////////////////////////////////////////
+    // Constructor.
+    //////////////////////////////////////////////////////////////////////
+    constructor() {
+        this.classNameAccordionBox = 'accordion-box';
+        this.classNameAccordionBoxHeading = 'heading';
+        this.classNameAccordionBoxHasContent = 'has-content';
+        this.classNameAccordionBoxContent = 'content';
+        this.classNameAccordionBoxItem = 'item';
+        this.classNameActive = 'active';
+    }
+
+
+    //////////////////////////////////////////////////////////////////////
+    // Run.
+    //////////////////////////////////////////////////////////////////////
+    Run() {
+        this.setUpForAccordingBox();
+    }
+
+
+    //////////////////////////////////////////////////////////////////////
+    // Set up for "according-box"
+    //////////////////////////////////////////////////////////////////////
+    async setUpForAccordingBox() {
+        const accordionBoxElms = document.querySelectorAll(`.${this.classNameAccordionBox}`);
+        for(let i = 0; i < accordionBoxElms.length; i++) {
+            const elms = accordionBoxElms[i].querySelectorAll(`.${this.classNameAccordionBoxHeading}.${this.classNameAccordionBoxHasContent}`);
+            if(elms === undefined || elms === null) {
+                continue;
+            }
+            for(let j = 0; j < elms.length; j++) {
+                const elm = elms[j].parentElement;
+                if(elm === undefined || elm === null || !elm.classList.contains(this.classNameAccordionBoxItem)) {
+                    continue;
+                }
+                const contentElm = elm.querySelector(`.${this.classNameAccordionBoxContent}`);
+                if(contentElm === undefined || contentElm === null) {
+                    continue;
+                }
+                elms[j].addEventListener('click', async () => {
+                    elm.classList.toggle(this.classNameActive);
+                    if(elm.classList.contains(this.classNameActive)) {
+                        contentElm.style.maxHeight = `${contentElm.scrollHeight}px`;
+                    } else {
+                        contentElm.style.maxHeight = '0px';
+                    }
+                });
+            }
+        }
+    }
+
+}
+
+
+//////////////////////////////////////////////////////////////////////
+// Initialize.
+//////////////////////////////////////////////////////////////////////
+document.addEventListener('readystatechange', (event) => {
+  if(event.target.readyState === 'complete') {
+    new Neumorphism().Run();
+  }
+});
